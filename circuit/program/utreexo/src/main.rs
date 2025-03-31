834746d712f52235a35a2ff8dd7b5a2f2533aefd
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2025-03-31 19:21:08 +0200

    feat: add native build support and input handling for utreexo program

--- circuit/program/utreexo/src/main.rs
+++ circuit/program/utreexo/src/main.rs
@@ -1,13 +1,18 @@
 //SPDX-License-Identifier: MIT
-#![no_main]
+#![cfg_attr(not(feature = "native"), no_main)]
+
+#[cfg(not(feature = "native"))]
 sp1_zkvm::entrypoint!(main);
 
-use alloy_sol_types::{sol, SolType};
-use bitcoin::{Block, TxIn};
-use rustreexo::accumulator::node_hash::BitcoinNodeHash;
-use rustreexo::accumulator::pollard::Pollard;
 use std::collections::HashMap;
 use std::ops::Deref;
+
+use alloy_sol_types::sol;
+use alloy_sol_types::SolType;
+use bitcoin::Block;
+use bitcoin::TxIn;
+use rustreexo::accumulator::node_hash::BitcoinNodeHash;
+use rustreexo::accumulator::pollard::Pollard;
 
 mod btc_structs;
 mod process_block;
@@ -21,23 +26,86 @@ type PublicValuesTuple = sol! {
 };
 
 pub fn main() {
-    let block = sp1_zkvm::io::read::<Block>();
-    let height = sp1_zkvm::io::read::<u32>();
-
-    let mut acc = sp1_zkvm::io::read::<Pollard>();
-    let input_leaf_hashes = sp1_zkvm::io::read::<HashMap<TxIn, BitcoinNodeHash>>();
-    let _proof = process_block(&block, height, &mut acc, input_leaf_hashes);
+    let (block, height, mut acc, input_leaf_hashes) = read_inputs();
+    let _proof = process_block(
+        &block,
+        height,
+        &mut acc,
+        input_leaf_hashes,
+    );
     let acc_roots: Vec<BitcoinNodeHash> = acc
         .get_roots()
-        .to_vec()
         .iter()
         .map(|rc| rc.get_data())
         .collect();
-    // TODO: have _proof and _compact_leaves as outputs instead of this
-    let acc_roots_bytes: Vec<[u8; 32]> = acc_roots.iter().map(|hash| *hash.deref()).collect();
+    let acc_roots_bytes: Vec<[u8; 32]> = acc_roots
+        .iter()
+        .map(|hash| *hash.deref())
+        .collect();
     let acc_roots_bytes_flat: Vec<u8> = acc_roots_bytes.concat();
 
     let bytes = PublicValuesTuple::abi_encode(&(acc_roots_bytes_flat,));
+    commit_slice(&bytes);
+}
 
-    sp1_zkvm::io::commit_slice(&bytes);
+#[cfg(feature = "native")]
+fn read_inputs() -> (
+    Block,
+    u32,
+    Pollard,
+    HashMap<TxIn, BitcoinNodeHash>,
+) {
+    use std::io::Read;
+    use std::io::{self};
+
+    use atty::Stream;
+    use serde_json;
+
+    // If stdin is a tty, then likely no piped input was provided.
+    if atty::is(Stream::Stdin) {
+        eprintln!("Error: No piped input provided (stdin is a tty).");
+        std::process::exit(1);
+    }
+
+    let mut input_data = String::new();
+    io::stdin()
+        .read_to_string(&mut input_data)
+        .expect("Failed to read from stdin");
+
+    if input_data.trim().is_empty() {
+        eprintln!("Error: Received empty input.");
+        std::process::exit(1);
+    }
+
+    serde_json::from_str(&input_data)
+        .expect("Deserialization failed: Provided input is invalid or cannot be parsed into the required types")
+}
+
+#[cfg(not(feature = "native"))]
+fn read_inputs() -> (
+    Block,
+    u32,
+    Pollard,
+    HashMap<TxIn, BitcoinNodeHash>,
+) {
+    (
+        sp1_zkvm::io::read::<Block>(),
+        sp1_zkvm::io::read::<u32>(),
+        sp1_zkvm::io::read::<Pollard>(),
+        sp1_zkvm::io::read::<HashMap<TxIn, BitcoinNodeHash>>(),
+    )
+}
+
+#[cfg(feature = "native")]
+fn commit_slice(bytes: &[u8]) {
+    use std::io::Write;
+    use std::io::{self};
+    io::stdout()
+        .write_all(bytes)
+        .unwrap();
+}
+
+#[cfg(not(feature = "native"))]
+fn commit_slice(bytes: &[u8]) {
+    sp1_zkvm::io::commit_slice(bytes);
 }
